0b6e369a81fe40facaf2cb4043d7af23c55004be
JTMaher2 <jtmaher2@gmail.com>
2016-06-27 23:06:19 -0500

    switched over to CDN Bootstrap

--- index.php
+++ index.php
@@ -5,14 +5,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Index</title>
-  <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
 <?php
 session_start();
 
 require "header.php";
-require "vendor/autoload.php";
 
 echo "<div class='container'>
         <div class='center jumbotron'>
