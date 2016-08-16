d175b3345132d2e78767f60977f4461e37ef768c
james <jtmaher2@gmail.com>
2016-08-16 18:16:12 -0500

    added 'Made with <3 in Wheaton' to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer class="footer">
     <small>
         PC Showcase by <a href="https://github.com/JTMaher2/">JTMaher2</a>
-        <br>© 2016
+        <br>© 2016<br>
+        Made with ♥ in Wheaton
     </small>
 </footer>
